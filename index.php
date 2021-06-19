3e35d47739a15827eb3137d69f41ba603189ae24
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 07:24:19 -0700

    Skeleton Design

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <?php
-      include '/head.php';
+      include 'head.php';
      ?>
     <title>Login</title>
   </head>
   <body>
-
+    <div class='center-page'>
+      <p>Login</p>
+      <p>Username: <input type='text' /></p><br/>
+      <p>Password: <input type='text' /></p>
+    </div>
   </body>
 </html>
