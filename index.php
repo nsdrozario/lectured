c143570052823299baea2b892dbe1c76aa72ad6a
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 07:49:16 -0700

    Fixed styling issues

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <div class='center-page'>
       <p>Login</p><br/><br/>
       <form action='login_auth.php'>
-        <p><label for='username'>Username:</label> <input name='username' type='text' id='username' /></p><br/>
-        <p><label for='password'>Password:</label> <input name='password' type='text' id='password' /></p><br/><br/>
+        <p>Username:&nbsp; <input name='username' type='text' id='username' /></p><br/>
+        <p>Password: <input name='password' type='text' id='password' /></p><br/><br/>
         <input type='submit' value='Submit' id='submit' />
       </form>
     </div>
