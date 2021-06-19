da0470d0b45b8e8f29ccf4f9fe54b0e998bce96a
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 13:49:25 -0700

    Optimized random number generator

--- create_course.php
+++ create_course.php
@@ -1,11 +1,3 @@
-<?php
-  $numbers = '0123456789';
-  $length = strlen($numbers);
-  $randomNumber = '';
-  for($i = 0; i < 6; i++) {
-    $randomNumber .= $characters[rand(0, $length - 1)];
-  }
- ?>
 <div class='modal'>
   <div class='modal-dialog'>
     <div class='modal-content'>
@@ -14,7 +6,7 @@
       </div>
       <div class='modal-body'>
         <p>Course Name: <input type='text' class='button' name='newCourseName' id='newCourseName' /></p>
-        <p>Join Code: <?php echo $randomNumber; ?></p>
+        <p>Join Code: <?php echo random_int(100000, 999999) ?></p>
       </div>
       <div class='modal-footer'>
         <button>Create</button>
