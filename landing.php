adf8bc4bc6fa7e94970d4843cd0806afbdcf7b16
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 09:08:03 -0700

    Initialized landing page

---
+++ landing.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php
+      include 'head.php';
+     ?>
+    <title>InspirEd - Home</title>
+  </head>
+  <body>
+    <div class='container home center-page'>
+      <div class='row'>
+        <div class='col-md-9'>
+          <p>Classes</p><br/>
+          <div class='classes'>
+            <p class='name-of-class'>AP Physics 1</p>
+            <p class='additional-links'>Assignments | Files</p>
+          </div><br/>
+          <div class='classes'>
+            <p class='name-of-class'>AP English Language and Composition</p>
+            <p class='additional-links'>Assignments | Files</p>
+          </div><br/>
+          <div class='classes'>
+            <p class='name-of-class'>AP Calculus AB</p>
+            <p class='additional-links'>Assignments | Files</p>
+          </div><br/>
+        </div>
+        <div class='col-md-3'>
+          <p>To-Do</p><br/>
+          <div class='assignments'>
+            <p class='close-assignment'>×</p><br/>
+            <div style='margin-top: 16px;'>
+              <p class='name-of-assignment'>007AB</p>
+              <p class='assignment-details'>AP Calculus AB</p>
+              <p class='assignment-details'>Due: March 14</p>
+            </div>
+          </div><br/>
+          <div class='assignments'>
+            <p class='close-assignment'>×</p><br/>
+            <div style='margin-top: 16px;'>
+              <p class='name-of-assignment'>Banneker Essay</p>
+              <p class='assignment-details'>AP English Language and Composition</p>
+              <p class='assignment-details'>Due: June 9th</p>
+            </div>
+          </div><br/>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
