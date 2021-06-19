a422d55a6751215501696c4f9ad846ac2aeea160
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-06-19 10:13:09 -0700

    change login verification

--- landing.php
+++ landing.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION["username"]) && isset($_SESSION["id"])) {
+if (!empty($_SESSION["username"]) && !empty($_SESSION["id"])) {
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
