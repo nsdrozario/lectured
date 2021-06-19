3ff93f01bab32fbf0365a4a44404a1c091fdb4e5
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-06-19 10:40:16 -0700

    the login "works locally"

--- login_auth.php
+++ login_auth.php
@@ -6,14 +6,13 @@ get user post data here from login form to authenticate
 
 require "include/user.php";
 
-if (isset($_POST["username"]) && isset($_POST["password"])) {
+if (isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["password"])) {
     $connection = pg_connect(getenv("DATABASE_URL"));
-    echo $conn;
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $result = pg_query_params($connection, "select password from public.users where name=$1", array(username));
-    $id = pg_query_params($connection,"select id from public.users where name=$1", array(username));
+    $result = pg_query_params($connection, "select password from public.users where name=$1", array($username));
+    $id = pg_query_params($connection,"select id from public.users where name=$1", array($username));
     if (password_verify($password, $result)) {
         // login successful
         
