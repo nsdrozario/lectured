70fc8eb22aed976ea0f7830c6e683e8179a90e4f
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 19:32:35 -0700

    Few things

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     }
      ?>
     <div class='center-page'>
-      <p>Login</p><br/><br/>
+      <p class='titular'>Login</p><br/><br/>
       <form action='login_auth.php' method='post'>
         <p>Username:&nbsp; <input name='username' type='text' id='username' /></p><br/>
         <p>Password: <input name='password' type='password' id='password' /></p><br/><br/>
