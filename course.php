4eecda2872689901aa615ea99c60b73d91d98631
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-06-19 19:15:40 -0700

    adapt name

--- course.php
+++ course.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    require "include/util.php";
+    if (!$_SESSION["id"]) {
+        exit();
+    }
+    if (!input_check($_GET["courseid"])) {
+        header("Location: account.php");
+    } else { 
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -8,7 +18,19 @@
   </head>
   <body>
     <div class='container home relative-top'>
-      <h1>AP Calculus AB</h1>
+      <h1>
+        <?php
+            $c = pg_connect(getenv("DATABASE_URL"));
+            $name = pg_query_params($c, "select course_title from public.courses where id=$1", array($_POST["courseid"]));
+            if (pg_num_rows($name) > 0) {
+                $name_arr = pg_fetch_assoc($name);
+                $real_name = $name_arr["course_title"];
+                if ($real_name != "") {
+                    echo $real_name;
+                }
+            }
+        ?>
+      </h1>
       <div style='text-align: left'>
         <p class='titular'>Quizzes</p>
         <div class='assignments' style='text-align: center'>
@@ -42,3 +64,6 @@
     </div>
   </body>
 </html>
+<?php
+    }
+?>
