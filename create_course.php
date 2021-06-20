3fd548032a67f81e52b5583a80c22a16bd75069d
shuklanishchal <nishchal.shukla1@gmail.com>
2021-06-19 17:07:27 -0700

    initial quiz creator

--- create_course.php
+++ create_course.php
@@ -1,8 +1,8 @@
 <p><input id='course-button' class='button' type='button' name='createCourse' value='Create a Course' onclick='courseCreator()' /></p>
 <div id='creator' style='display: none'>
-<p>Course Name: <input style='margin-left: 20px;' type='text' class='button' name='newCourseName' id='newCourseName' /></p>
-<div id="feedback"></div>
-<input type='button' value='Create This Course' class='button' name='createCourse' onclick='request_course_creation()' />
+  <p>Course Name: <input style='margin-left: 20px;' type='text' class='button' name='newCourseName' id='newCourseName' /></p>
+  <div id="feedback"></div>
+  <input type='button' value='Create This Course' class='button' name='createCourse' onclick='request_course_creation()' />
 </div>
 <style>
   #join-class {
